67ec856b097a7527eb79a4f7068143ca56640b03
Itay.Talias <itay.talias@gmail.com>
2022-09-21 18:36:34 +0300

    adding class player

---
+++ backend/player.py
@@ -0,0 +1,9 @@
+
+class Player:
+
+    def __init__(self, player_dict):
+        self.first_name = player_dict.get("firstName")
+        self.last_name = player_dict.get("lastName")
+        self.team_id = player_dict.get("teamId")
+        self.position = player_dict.get("pos")
+        self.jersey_number = player_dict.get("jersey")
