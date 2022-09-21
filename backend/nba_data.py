55df4975968a094bc185d8886694c381309c730a
Itay.Talias <itay.talias@gmail.com>
2022-09-21 18:41:25 +0300

    adding nba data class

---
+++ backend/nba_data.py
@@ -0,0 +1,28 @@
+import numpy as np
+from requests_nba_server import Requests_nba_server
+from player import Player
+from team_id import teams_id
+
+
+class NBA_data:
+
+    def __init__(self):
+        self.leagues_arr = []
+        self.year = 0
+
+    def fetch_leagues_by_year(self, year):
+        self.leagues_arr = Requests_nba_server.get_nba_leagues_by_year(year)
+        self.year = year
+
+    def get_players_arr(self):
+        if len(self.leagues_arr) == 0:
+            raise ValueError("The leagues array is empty")
+        else:
+            player_arr = []
+            for league in self.leagues_arr:
+                player_arr += [Player(player)
+                               for player in self.leagues_arr[league]]
+            return player_arr
+
+    # def get_players_arr_by_team_name(self,team_name):
+    #     return filter(lambda player)
