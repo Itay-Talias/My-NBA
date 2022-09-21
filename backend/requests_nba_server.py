d781326f792ec6b38052fa102b7092b9b9d83474
Itay.Talias <itay.talias@gmail.com>
2022-09-21 18:41:42 +0300

    adding requests class

---
+++ backend/requests_nba_server.py
@@ -0,0 +1,10 @@
+import requests
+
+
+class Requests_nba_server:
+    @staticmethod
+    def get_nba_leagues_by_year(year):
+        res = requests.get(
+            f"http://data.nba.net/10s/prod/v1/{year}/players.json")
+        print(res)
+        return res.json().get("league")
