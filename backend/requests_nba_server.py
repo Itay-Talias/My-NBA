412263f3b500bb6908c00b9e1e450e711d59cc2b
Itay.Talias <itay.talias@gmail.com>
2022-09-29 18:27:22 +0300

    get picture by full name

--- backend/requests_nba_server.py
+++ backend/requests_nba_server.py
@@ -7,3 +7,9 @@ class Requests_nba_server:
         res = requests.get(
             f"http://data.nba.net/10s/prod/v1/{year}/players.json")
         return res.json().get("league")
+
+    @staticmethod
+    def get_picture_by_player_name(first_name, last_name):
+        res = requests.get(
+            f"https://nba-players.herokuapp.com/players/{last_name}/{first_name}")
+        return res
