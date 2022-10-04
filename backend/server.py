1cf662ab9b1808d4ac1f54f8b60c15ad55043bd1
Itay.Talias <itay.talias@gmail.com>
2022-10-04 14:30:10 +0300

    dream team end-points

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,14 @@
-from http import HTTPStatus
-from fastapi import FastAPI, Request, Response
+import imp
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 import uvicorn
 from nba_data import NBA_data
+from dream_team import Dream_team
 
 app = FastAPI()
+nba_data = NBA_data()
+dream_team = Dream_team()
 
 app.mount("/fronted", StaticFiles(directory="fronted"), name="fronted")
 
@@ -17,10 +20,27 @@ def root():
 
 @app.get('/players/{team_name}/{year}')
 def get_players(team_name, year):
-    data = NBA_data()
-    data.fetch_leagues_by_year(year)
-    return data.get_players_arr_by_team_name(team_name)
+    nba_data.fetch_leagues_by_year(year)
+    return nba_data.get_players_arr_by_team_name(team_name)
+
+
+@app.get('/dream_team')
+def get_dream_team():
+    return dream_team.get_dream_team()
+
+
+@app.post('/dream_team')
+async def add_player_to_dream_team(request: Request):
+    res = await request.json()
+    dream_team.add_player_to_dream_team(res)
+
+# @app.put('/dream_team')
+# def update_player_in_dream_team():
+
+
+# @app.delete('/dream_team')
+# def delete_player_from_dream_team():
 
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
