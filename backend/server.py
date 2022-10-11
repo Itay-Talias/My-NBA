983dcf0d42ea8240a84c9605982a04bde5226c8b
Itay.Talias <itay.talias@gmail.com>
2022-10-11 09:11:38 +0300

    change port

--- backend/server.py
+++ backend/server.py
@@ -58,4 +58,4 @@ async def delete_player_from_dream_team(request: Request):
 
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=3000, reload=True)
+    uvicorn.run("server:app", host="0.0.0.0", port=4000, reload=True)
